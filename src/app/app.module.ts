f84643d08e8d01711a44a5df08ac7b246220be96
Leonsaari <joonas.leonsaari@siemens.com>
2021-03-11 18:22:13 +0200

    feat(frontend): added Angular Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
+/* Modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './modules/angular.material.module';
 
+/* Global Components */
+
+
+/* Home page components */
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
