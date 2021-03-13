fff4a258e37d12af6f3446218dfd17c5b2fd807a
Leonsaari <joonas.leonsaari@siemens.com>
2021-03-13 10:57:39 +0200

    added comments to service functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
-import { StorageService } from './services/storage.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
